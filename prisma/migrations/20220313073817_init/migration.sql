44dd48b7fb604df6086f5e38839482a19878918f
citReyJoshua <reyjoshua.macarat@cit.edu>
2022-03-13 16:37:24 +0800

    chore: updated description field (optional) in schema

--- prisma/migrations/20220313073817_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,classId]` on the table `Enrolment` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Enrolment_userId_classId_key" ON "Enrolment"("userId", "classId");
