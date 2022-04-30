3ead1f353c241fc7b8f7ebb42b1e1ad05c62d8bc
citReyJoshua <reyjoshua.macarat@cit.edu>
2022-04-30 16:52:14 +0800

    chore: removed finished exam late, entered exam early activity type

---
+++ prisma/migrations/20220430085041_init/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [FINISHED_EXAM_FAST,ENTERED_EXAM_LATE] on the enum `ActivityName` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ActivityName_new" AS ENUM ('SWITCHED_TAB', 'LOSE_WINDOW_FOCUS', 'USED_PASTE_FUNCTION', 'ACCESSED_SITE', 'USED_SEARCH_ENGINE', 'FINISHED_EXAM', 'WENT_IDLE', 'JOINED_EXAM', 'RETURNED');
+ALTER TABLE "Activity" ALTER COLUMN "name" TYPE "ActivityName_new" USING ("name"::text::"ActivityName_new");
+ALTER TYPE "ActivityName" RENAME TO "ActivityName_old";
+ALTER TYPE "ActivityName_new" RENAME TO "ActivityName";
+DROP TYPE "ActivityName_old";
+COMMIT;
