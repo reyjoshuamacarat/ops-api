b04b032fea259874c805a450469a7186cf8098b5
citReyJoshua <reyjoshua.macarat@cit.edu>
2022-05-25 16:33:25 +0800

    chore: port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
   app.enableCors()
-  await app.listen(8000)
+  await app.listen(7809)
 }
 bootstrap()
